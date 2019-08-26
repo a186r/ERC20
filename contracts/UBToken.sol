198cfcbcbd28027c11b55cea529922ad14510e70
a186r <hi.aewalle@gmail.com>
2019-08-26 14:32:20 +0800

    更新一下

--- contracts/UBToken.sol
+++ contracts/UBToken.sol
@@ -1,7 +1,7 @@
-pragma solidity ^0.5.0;
+pragma solidity >=0.4.21 <0.6.0;
 
-import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 
 contract UBToken is ERC20, ERC20Detailed {
     constructor(uint256 initialSupply) ERC20Detailed("UBToken", "UBT", 18) public {
